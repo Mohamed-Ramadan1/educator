3b1ab1bc81bff7a67d3d79c1a3fd7b7415219a39
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2025-05-19 07:43:43 +0300

    enhance and setup project main structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
